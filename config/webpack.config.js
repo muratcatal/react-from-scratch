449b882bad9ea15d7b6282cf6768b58f17f083d1
Murat ÇATAL <muratcatal@outlook.com>
2018-05-07 13:37:15 +0300

    webpack configuration file moved to config folder

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: "./src/index.tsx",
     output: {
-        path: path.resolve(__dirname, "public"),
+        path: path.resolve(__dirname,"../", "public"),
         filename: "bundle.js"
     },
     resolve: {
@@ -12,7 +12,7 @@ module.exports = {
     devServer: {
         inline: true,
         port: 8080,
-        contentBase: path.join(__dirname, "public"),
+        contentBase: path.join(__dirname,"../", "public"),
         open: true,
         overlay: true, // shows when an error occurrs on page.
         progress: true,
